c74c888e45d001492114190b2b8f2cf2ec54f18a
Matti Paksula <matti.paksula@iki.fi>
2020-08-21 19:19:07 +0300

    -debug

--- src/main.go
+++ src/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"os"
 	"os/exec"
 	"strings"
@@ -11,31 +14,47 @@ import (
 )
 
 func main() {
-	if len(os.Args) < 5 {
+	var debug bool
+	flag.BoolVar(&debug, "debug", false, "debug")
+	flag.Parse()
+
+	if !debug {
+		log.SetOutput(ioutil.Discard)
+	}
+
+	if len(flag.Args()) < 4 {
 		fmt.Println("usage: tail-debounce-exec-greps file debounceDuration command grep...")
 		os.Exit(1)
 	}
 
-	filename := os.Args[1]
-	duration, err := time.ParseDuration(os.Args[2])
+	filename := flag.Args()[0]
+	duration, err := time.ParseDuration(flag.Args()[1])
 	if err != nil {
 		panic(err)
 	}
-	cmd := strings.Fields(os.Args[3])
-	greps := os.Args[4:]
+	cmd := strings.Fields(flag.Args()[2])
+	greps := flag.Args()[3:]
+
+	tailLogger := tail.DiscardingLogger
+	if debug {
+		tailLogger = tail.DefaultLogger
+	}
 
 	t, _ := tail.TailFile(filename, tail.Config{
 		ReOpen:    true,
 		MustExist: false,
 		Follow:    true,
-		Logger:    tail.DiscardingLogger,
+		Logger:    tailLogger,
 	})
 
 	lastExec := time.Now()
 	for line := range t.Lines {
+		log.Println(line.Text)
 		for _, grep := range greps {
 			if strings.Contains(line.Text, grep) {
+				log.Printf("matches %s", grep)
 				if time.Now().Sub(lastExec) < duration {
+					log.Printf("debounce")
 					continue
 				}
 				lastExec = time.Now()
@@ -46,6 +65,7 @@ func main() {
 
 				switch err := cmd.Run(); err.(type) {
 				case nil, *exec.ExitError:
+					log.Printf("exec err %v", err)
 				default:
 					panic(err)
 				}
